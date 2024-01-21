3a86f6e70b9d94a8d4854fa6b0e3590aceeb89ad
morriswa <william@morriswa.org>
2024-01-21 12:53:45 -0600

    # replace DatasourceConfig builder pattern with procedural pattern

--- src/main/java/org/morriswa/config/DatasourceConfig.java
+++ src/main/java/org/morriswa/config/DatasourceConfig.java
@@ -1,8 +1,10 @@
 package org.morriswa.config;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -18,18 +20,45 @@ import org.springframework.core.env.Environment;
 @Configuration
 @Profile("!test")
 public class DatasourceConfig {
-    @Autowired private Environment e;
 
+    private final Environment e;
+
+    private final Logger log;
+
+
+    @Autowired public DatasourceConfig(Environment e) {
+        this.e = e;
+        this.log = LoggerFactory.getLogger(DatasourceConfig.class);
+    }
+
+
+    /**
+     * Attempts to build the Hikari Datasource to be used by the application for data-access
+     * If datasource cannot be configured, service will halt
+     *
+     * @return a HikariDataSource bean that will be used by JDBC for Data Access
+     */
     @Bean
     public HikariDataSource provideHikariDataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class)
-            .username(e.getRequiredProperty("mysql.database.username"))
-            .password(e.getRequiredProperty("mysql.database.password"))
-            .url(String.format("%s://%s:%s/%s",
-                    e.getRequiredProperty("mysql.scheme"),
-                    e.getRequiredProperty("mysql.path"),
-                    e.getRequiredProperty("mysql.port"),
-                    e.getRequiredProperty("mysql.database.name"))
-            ).build();
+
+        final var dbUsername = e.getRequiredProperty("mysql.database.username");
+        final var dbPassword = e.getRequiredProperty("mysql.database.password");
+        final var jdbcUrl = String.format("%s://%s:%s/%s",
+                e.getRequiredProperty("mysql.scheme"),
+                e.getRequiredProperty("mysql.path"),
+                e.getRequiredProperty("mysql.port"),
+                e.getRequiredProperty("mysql.database.name"));
+
+        var databaseConfig = new HikariConfig();
+        databaseConfig.setUsername(dbUsername);
+        databaseConfig.setPassword(dbPassword);
+        databaseConfig.setJdbcUrl(jdbcUrl);
+
+        try {
+            return new HikariDataSource(databaseConfig);
+        } catch (Exception e) {
+            log.error("Could not configure database! Encountered the following errors, shutting down",e);
+            throw new RuntimeException(e);
+        }
     }
 }
