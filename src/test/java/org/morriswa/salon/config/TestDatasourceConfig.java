6b261fce49f7e369db9d80cc10ad20f65fb4e6f4
morriswa <william@morriswa.org>
2024-03-30 14:05:04 -0500

    # move test fb config to yaml

--- src/test/java/org/morriswa/salon/config/TestDatasourceConfig.java
+++ src/test/java/org/morriswa/salon/config/TestDatasourceConfig.java
@@ -5,36 +5,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @TestConfiguration
-@EnableTransactionManagement
-@EnableJdbcRepositories(basePackages = {
-        "org.morriswa.salon.dao"
-})
 public class TestDatasourceConfig {
 
-    private final Environment e;
+    private final MySQLProperties mysql;
 
     @Autowired
-    public TestDatasourceConfig(Environment e) {
-        this.e = e;
+    public TestDatasourceConfig(MySQLProperties mysql) {
+        this.mysql = mysql;
     }
 
     @Bean @Profile("test")
     public HikariDataSource getTestDatasource() {
         var dataSource = new HikariDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setJdbcUrl(e.getRequiredProperty("jdbc.url"));
-        dataSource.setUsername(e.getRequiredProperty("jdbc.username"));
-        dataSource.setPassword(e.getRequiredProperty("jdbc.password"));
+
+        StringBuilder connectionString = new StringBuilder(
+            String.format("%s:%s",
+                mysql.getProtocol(),
+                mysql.getDatabase())
+        );
+        final String jdbcUrl;
+
+        // if additional connection params are required by application
+        if (!mysql.getConnectionProperties().isEmpty()) {
+            // add all params
+            connectionString.append(";");
+            for (String prop : mysql.getConnectionProperties())
+                connectionString.append(String.format("%s;", prop));
+        }
+
+        jdbcUrl = connectionString.toString();
+
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUsername(mysql.getUsername());
+        dataSource.setPassword(mysql.getPassword());
 
         return dataSource;
     }
